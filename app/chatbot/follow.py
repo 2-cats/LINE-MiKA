c473a4d7806128e96d8fe0eee668e3568d173050
Eric <smart032410@gmail.com>
2019-01-22 17:21:29 +0800

    Liff router

--- app/chatbot/follow.py
+++ app/chatbot/follow.py
@@ -1,2 +1,43 @@
+from linebot.models import (BoxComponent, BubbleContainer, ButtonComponent,
+                            FlexSendMessage, TextComponent, URIAction)
+
+
 def follow_message():
-    return 0
+    bubble_template = BubbleContainer(
+        body=BoxComponent(
+            layout='vertical',
+            contents=[
+                TextComponent(
+                     text='歡迎加入',
+                    wrap=True,
+                    weight= 'bold',
+                    size='lg',
+                ),
+                TextComponent(
+                    text='您好，我是 CardGO！你可以叫我卡狗，我的任務是努力為大家整理、搜尋名片、連結群組上的每位朋友！第一次使用，不妨先傳一張名片讓我認識你吧！',
+                    wrap=True,
+                    size='md',
+                    margin='md'
+                )
+            ]
+        ),
+        footer=BoxComponent(
+            layout='vertical',
+            spacing="sm",
+            contents=[
+                ButtonComponent(
+                    style='link',
+                    height='sm',
+                    action=URIAction(label='智慧新增名片', uri='line://nv/camera/'),
+                ),
+                ButtonComponent(
+                    style='link',
+                    height='sm',
+                    action=URIAction(label='手動新增名片', uri='https://google.com/'),
+                )
+            ]
+        )
+    )
+    message = FlexSendMessage(
+        alt_text='抱歉，我聽不懂指令', contents=bubble_template)
+    return message
--- app/liff/views.py
+++ app/liff/views.py
@@ -13,3 +13,18 @@ app.config.from_pyfile('config.py')
 line_bot_api = LineBotApi(app.config["LINE_CHANNEL_ACCESS_TOKEN"])
 handler = WebhookHandler(app.config["LINE_CHANNEL_SECRET"])
 
+@liff.route("/line/auto_add_card", methods=['GET'])
+def line_auto_add_card():
+    data=request.args.get('data')
+    return render_template(
+        'line/auto_add_card.html',
+        card_data=data
+    )
+
+@liff.route("/line/add_card_manual", methods=['GET'])
+def line_manual_add_card():
+    return render_template('line/manual_add_card.html')
+
+@liff.route("/line/add_activity", methods=['GET'])
+def line_add_activity():
+    return render_template('line/add_activity.html')
