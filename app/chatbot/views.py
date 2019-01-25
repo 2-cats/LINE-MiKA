98ee8cd9c5c471eca997ad7ff9e66aab71010ff0
tommy <tommy.chen@goodlinker.io>
2019-01-25 17:28:11 +0800

    個人新增活動

--- app/chatbot/views.py
+++ app/chatbot/views.py
@@ -14,7 +14,7 @@ from . import chatbot
 from .. import db
 from .activity import (add_group_activity_message, group_activity_message,
                        join_group_activity_message, my_activity_message,
-                       who_join_group_activity_message)
+                       who_join_group_activity_message,add_activity_message)
 from .card import (card_management_message, delete_my_card_message,
                    search_card_message, show_my_card_message)
 from .error_message import alert_no_action_message
@@ -73,6 +73,10 @@ def handle_message(event):
     if event.source.type == 'user':
         if message_text == "名片管理":
             message = card_management_message(line_user_id)
+            line_bot_api.reply_message(event.reply_token, message)
+            return 0
+        if message_text == "新增活動":
+            message = add_activity_message(line_user_id)
             line_bot_api.reply_message(event.reply_token, message)
             return 0
         elif message_text == "我的活動":
