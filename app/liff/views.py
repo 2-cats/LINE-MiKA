150e7c2e6faac6eaa6209e3b8758b434601d5337
Eric <smart032410@gmail.com>
2019-01-28 10:08:20 +0800

    report card

--- app/liff/views.py
+++ app/liff/views.py
@@ -7,7 +7,7 @@ from linebot.models import (AudioMessage, FollowEvent, ImageMessage,
 
 from . import liff
 from .. import db
-from .card import add_card
+from .card import add_card, report_card_issue
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
@@ -30,3 +30,16 @@ def line_add_card_success():
 @liff.route("/line/add_activity", methods=['GET'])
 def line_add_activity():
     return render_template('line/add_activity.html')
+
+@liff.route("/line/report_card", methods=['GET'])
+def line_report_card():
+    data = request.args.to_dict()
+    return render_template(
+        'line/report_card.html',
+        data=data
+    )
+
+@liff.route("/line/report_card", methods=['POST'])
+def line_report_card_success():
+    report_card_issue(request.form.to_dict())
+    return render_template('line/report_card_success.html')
