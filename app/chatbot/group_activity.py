0f56f8600199a8ae79195b7867d8408786da37e3
Eric <smart032410@gmail.com>
2019-03-04 11:15:56 +0800

    Card show rel link

--- app/chatbot/group_activity.py
+++ app/chatbot/group_activity.py
@@ -288,7 +288,7 @@ def join_group_activity_message(activity_id, line_user_id):
                         size='md',
                     ),
                     TextComponent(
-                        text='請先跟咪卡我成為好友，才能加入活動喔',
+                        text='請先跟咪卡我成為好友，才能加入活動，加我好友後，還可以快速方便的查詢群組活動喔！',
                         margin='md',
                         wrap=True,
                         color='#666666',
@@ -423,7 +423,7 @@ def leave_group_activity_message(activity_id, line_user_id):
                         size='md',
                     ),
                     TextComponent(
-                        text='請先跟咪卡我成為好友，我才知道你加入了什麼活動喔',
+                        text='請先跟咪卡我成為好友，才能加入活動，加我好友後，還可以快速方便的查詢群組活動喔！',
                         margin='md',
                         wrap=True,
                         color='#666666',
